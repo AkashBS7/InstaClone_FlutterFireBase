6886292dd8ab1dce04e820ead37a4a4f348d9a51
Akash Behara <akashbehara33@gmail.com>
2022-03-04 22:16:03 +0530

    implemented Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,25 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:instaclone_flutterfirebase/responsive/mobile_screen_layout.dart';
 import 'package:instaclone_flutterfirebase/responsive/responsive_layout_screen.dart';
 import 'package:instaclone_flutterfirebase/responsive/web_screen_layout.dart';
 import 'package:instaclone_flutterfirebase/utils/colors.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+          apiKey: "AIzaSyB3U_sWVVud9glEPAg9RszEdZ-C94VAqMI",
+          appId: "1:702862956322:web:386dbc494902a2cf29777b",
+          messagingSenderId: "702862956322",
+          projectId: "instaclone-flutter-7a755",
+          storageBucket: "instaclone-flutter-7a755.appspot.com"),
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
   runApp(const MyApp());
 }
 
