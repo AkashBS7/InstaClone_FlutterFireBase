df332839e5da1942282ca71efb0a6d8f928e9642
Akash Behara <akashbehara33@gmail.com>
2022-03-10 21:00:38 +0530

    web screen update

--- lib/responsive/web_screen_layout.dart
+++ lib/responsive/web_screen_layout.dart
@@ -1,11 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:instaclone_flutterfirebase/utils/colors.dart';
 
-class WebScreenLayout extends StatelessWidget {
+class WebScreenLayout extends StatefulWidget {
   const WebScreenLayout({Key? key}) : super(key: key);
+
+  @override
+  State<WebScreenLayout> createState() => _WebScreenLayoutState();
+}
+
+class _WebScreenLayoutState extends State<WebScreenLayout> {
+  int _page = 0;
+
+  late PageController pageController;
+
+  @override
+  void initState() {
+    pageController = PageController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    pageController.dispose();
+    super.dispose();
+  }
+
+  void navigationTapped(int page) {
+    pageController.jumpToPage(page);
+  }
+
+  void onPageChanged(int page) {
+    setState(() {
+      _page = page;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: mobileBackgroundColor,
+        centerTitle: false,
+        title: SvgPicture.asset('assets/ic_instagram.svg',
+            color: primaryColor, height: 32),
+        actions: [
+          IconButton(onPressed: () {}, icon: Icon(Icons.home)),
+          IconButton(onPressed: () {}, icon: Icon(Icons.search)),
+          IconButton(onPressed: () {}, icon: Icon(Icons.add_a_photo)),
+          IconButton(onPressed: () {}, icon: Icon(Icons.favorite)),
+          IconButton(onPressed: () {}, icon: Icon(Icons.person)),
+        ],
+      ),
       body: Center(child: Text('I am WebScreen')),
     );
   }
